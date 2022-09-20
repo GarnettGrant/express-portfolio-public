37e74d0ced5d78ba4c43bf11721549175cb3f41b
Garnett Grant <net_grant@hotmail.com>
2022-09-20 10:45:41 -0400

    Reconstructed Boilerplate

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,34 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
 });
+
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+
+/* GET About us page. */
+router.get('/aboutus', function(req, res, next) {
+  res.render('index', { title: 'About Us' });
+});
+
+/* GET Products page. */
+router.get('/products', function(req, res, next) {
+  res.render('index', { title: 'Products' });
+});
+
+/* GET Services */
+router.get('/services', function(req, res, next) {
+  res.render('index', { title: 'Services' });
+});
+
+/* GET Contact us page. */
+router.get('/contactus', function(req, res, next) {
+  res.render('index', { title: 'Contact Us' });
+});
+
 
 module.exports = router;
