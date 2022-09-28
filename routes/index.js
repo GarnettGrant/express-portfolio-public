d2b6d6c05d869fe810a25f5e8c8941d7218a2313
Garnett Grant <net_grant@hotmail.com>
2022-09-28 18:40:53 -0400

    Updated MVC/Content

--- routes/index.js
+++ routes/index.js
@@ -1,35 +1,35 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Home' });
 });
 
 /* GET home page. */
-router.get('/home', function(req, res, next) {
+router.get('/home', function (req, res, next) {
   res.render('index', { title: 'Home' });
 });
 
 
 /* GET About me page. */
-router.get('/aboutme', function(req, res, next) {
-  res.render('index', { title: 'About Me' });
+router.get('/aboutme', function (req, res, next) {
+  res.render('aboutme', { title: 'About Me' });
 });
 
 /* GET Projects page. */
-router.get('/projects', function(req, res, next) {
-  res.render('index', { title: 'Projects' });
+router.get('/projects', function (req, res, next) {
+  res.render('projects', { title: 'Projects' });
 });
 
 /* GET Services */
-router.get('/services', function(req, res, next) {
-  res.render('index', { title: 'Services' });
+router.get('/services', function (req, res, next) {
+  res.render('services', { title: 'Services' });
 });
 
 /* GET Contact me page. */
-router.get('/contactme', function(req, res, next) {
-  res.render('index', { title: 'Contact Me' });
+router.get('/contactme', function (req, res, next) {
+  res.render('contact', { title: 'Contact Me' });
 });
 
 
